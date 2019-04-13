151a423769848da00a024987af1807b28fa970e4
王德昭 <455708864@qq.com>
2019-04-13 16:37:12 +0800

    Revert "spring jdbc操作数据库,连接vpn后按jdbc包的样例做"
    This reverts commit 9a066a26e7c22aa0af227a1d2fc10a148da1a55e.

--- digitalrecognition/src/main/java/org/dl4j/jdbc/ImageDao.java
+++
@@ -1,9 +0,0 @@
-package org.dl4j.jdbc;
-import java.util.List;
-import javax.sql.DataSource;
-public interface ImageDao {
-	public void setdatasource(DataSource ds);
-	public void addImage(Image image);
-	public List<Image> getLast10();
-
-}
