41279e8f1c0ed0b1781c65774ace288629d2094c
魏健 <1719003878@qq.com>
2019-04-14 23:09:28 +0800

    Revert "没用的更改"
    This reverts commit 2e50e70036f68c007ee8ed25ac7ab526fcee7213.

--- digitalrecognition/src/main/java/org/dl4j/jdbc/Image.java
+++ digitalrecognition/src/main/java/org/dl4j/jdbc/Image.java
@@ -2,18 +2,10 @@ package org.dl4j.jdbc;
 
 public class Image {
 	private String ImagePath;
-	private String Base64;
-	public Image() {}
-	public Image(String ImagePath,String Base64) {
-		this.ImagePath=ImagePath;
-		this.Base64=Base64;
-	}
 	
-	public void setBase64(String setBase64) {
-		this.Base64=setBase64;
-	}
-	public String getBase64() {
-		return this.Base64;
+	public Image() {}
+	public Image(String ImagePath) {
+		this.ImagePath=ImagePath;
 	}
 	
 	public void setImagePath(String ImagePath) {
@@ -23,7 +15,7 @@ public class Image {
 		return this.ImagePath;
 	}
 	public void display() {
-		System.out.println("Base64: "+this.Base64);
+		System.out.println("ImagePath: "+this.ImagePath);
 	}
 	
 }
--- digitalrecognition/src/main/java/org/dl4j/jdbc/ImageDaoImp.java
+++ digitalrecognition/src/main/java/org/dl4j/jdbc/ImageDaoImp.java
@@ -18,13 +18,13 @@ public class ImageDaoImp implements ImageDao {
 
 	public void addImage(Image image) {
 		//String sql="insert into class.Image(ImagePath) values (?)";
-		String sql="insert into image(ImagePath,Base64,InsertTime) VALUES(?,?,?)";
-		jdbcTemplateObject.update(sql,image.getImagePath(),image.getBase64(),new Date());
+		String sql="insert into image(ImagePath,InsertTime) VALUES(?,?)";
+		jdbcTemplateObject.update(sql,image.getImagePath(),new Date());
 	}
 
 	public List<Image> getLast10() {
 		List<Image> images=null;
-		String sql="select Base64 from image order by InsertTime Desc limit 10";
+		String sql="select ImagePath from image order by InsertTime Desc limit 10";
 		images=jdbcTemplateObject.query(sql, new ImageMapper());
 		return images;
 	}
